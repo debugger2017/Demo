778dde4fc7c7e88600a6b074041466d350ce40f4
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-13 11:58:30 +0530

    Created RegisteredUsers model,for details see migrations

--- demo_project/mail_system/admin.py
+++ demo_project/mail_system/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
-# Register your models here.
+from mail_system.models import RegisteredUsers
+admin.site.register(RegisteredUsers)
---
+++ demo_project/mail_system/migrations/0001_initial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='RegisteredUsers',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('mobile', models.CharField(max_length=10)),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
