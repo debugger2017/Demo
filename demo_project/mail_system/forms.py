3c7ba0de8ba3816874cbd840ca149f736907abfe
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-13 12:41:01 +0530

    Added forms and edited models file

---
+++ demo_project/mail_system/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+from mail_system.models import RegisteredUsers
+
+class UserForm(forms.ModelForm):
+	password = forms.CharField(widget = forms.PasswordInput())	
+	class Meta:
+		model = User	
+		fields = ('username','email','password')
+
+class RegisteredUsersForm(forms.ModelForm):
+	class Meta:
+		model = RegisteredUsers
+		fields = ('mobile')
--- demo_project/mail_system/models.py
+++ demo_project/mail_system/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class RegisteredUsers(models.Model):
 	user = models.OneToOneField(User)
+	#User._meta.get_field('email')._unique = True
 	mobile = models.CharField(max_length = 10)
 
 
