02837f0e8bce221eaaa2aeae97e69cf864ee52b9
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-13 18:52:06 +0530

    Added register method in view

--- demo_project/mail_system/views.py
+++ demo_project/mail_system/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from mail_system.forms import UserForm, RegisteredUsersForm
 
-# Create your views here.
+def register(request):
+	registered = False
+
+	if request.method == 'POST':
+		user_form = UserForm(data = request.POST)
+		registered_users_form = RegisteredUsersForm(data = request.POST)
+
+		if user_form.is_valid() and registered_users_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+
+			registered_users = registered_users_form.save(commit = False)
+			registered_users.user = user	
+			registered_users.save()
+			registered = True			
+		else:
+			print(user_form.errors , registered_users_form.errors)
+	else:
+		user_form = UserForm()
+		registered_users_form = RegisteredUsersForm()		
+
+	return render(request,
+            'mail_system/register.html',
+            {'user_form': user_form, 'registered_users_form': registered_users_form, 'registered': registered} )
+	
+		
