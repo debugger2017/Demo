f3747e409e28d6c037c046684ae03daa247b19a6
debugger2017 <kartik.p.satoskar@gmail.com>
2017-02-22 19:50:46 +0530

    Added Sign Up card

--- demo_project/demo_project/settings.py
+++ demo_project/demo_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mail_system',
+    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
--- demo_project/mail_system/forms.py
+++ demo_project/mail_system/forms.py
@@ -1,20 +1,21 @@
 from django import forms
-
-from django.contrib.auth.models import User
-from mail_system.models import RegisteredUsers
+from mail_system.models import Mail, RegisteredUsers
+from django.contrib.auth.models import User 
 
 class UserForm(forms.ModelForm):
-	password = forms.CharField(widget = forms.PasswordInput())	
+	password = forms.CharField(label="",widget = forms.PasswordInput(attrs={'class':"form-control",'placeholder':"Password"}))
+	username = forms.CharField(label="",widget = forms.TextInput(attrs={'class':"form-control",'placeholder':"Username"}))
+	email = forms.CharField(label="",widget = forms.TextInput(attrs={'class':"form-control",'placeholder':"Email"}))	
 	class Meta:
 		model = User	
 		fields = ('username','email','password')
 
 class RegisteredUsersForm(forms.ModelForm):
+	mobile = forms.CharField(label="",widget = forms.TextInput(attrs={'class':"form-control",'placeholder':"Mobile"}))
+	city = forms.CharField(label="",widget = forms.TextInput(attrs={'class':"form-control",'placeholder':"City"}))
 	class Meta:
 		model = RegisteredUsers
 		fields = ('mobile','city')
-
-from mail_system.models import Mail
 
 class MailForm(forms.ModelForm):
     class Meta:
