4b79fc10931cd7f9ac51922c37a93e0d518c4c32
debugger2017 <kartik.p.satoskar@gmail.com>
2017-03-07 12:03:05 +0530

    Pickle is modified

--- demo_project/mail_system/views.py
+++ demo_project/mail_system/views.py
@@ -135,7 +135,7 @@ def spam(request):
         for record in records:
             from_user = User.objects.get(id=record.sender_id)
             mail = Mail.objects.get(id=record.mail_id)
-            if mail.is_spam:
+            if mail.is_spam == True:
                 mails.append(mail)
         return render(request,
             'mail_system/inbox.html', {'current_user': current_user, 'records': records , 'mails':mails , 'from_user':from_user})
