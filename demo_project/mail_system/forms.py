bdf598e4114d7503c1ff712068e5493b07f4bcb3
debugger2017 <kartik.p.satoskar@gmail.com>
2017-04-01 12:39:56 +0530

    Updated forms

--- demo_project/mail_system/forms.py
+++ demo_project/mail_system/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from mail_system.models import Mail, RegisteredUsers
 from django.contrib.auth.models import User 
-from mail_system.models import RegisteredUsers, Mail
+from mail_system.models import Mail
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(label="",widget = forms.PasswordInput(attrs = {'placeholder': 'Password'}))
@@ -10,13 +9,6 @@ class UserForm(forms.ModelForm):
 	class Meta:
 		model = User	
 		fields = ('username','email','password')
-
-class RegisteredUsersForm(forms.ModelForm):
-	mobile = forms.CharField(label="",widget = forms.TextInput(attrs = {'placeholder': 'Mobile'}))
-	city = forms.CharField(label="",widget = forms.TextInput(attrs = {'placeholder': 'City'}))
-	class Meta:
-		model = RegisteredUsers
-		fields = ('mobile','city')
 
 class MailForm(forms.ModelForm):
     receiver = forms.CharField(label="",widget = forms.TextInput(attrs = {'placeholder': 'To'})) 
--- demo_project/mail_system/models.py
+++ demo_project/mail_system/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Mail(models.Model):
     subject = models.CharField(max_length = 256)
     content = models.CharField(max_length = 4096)
